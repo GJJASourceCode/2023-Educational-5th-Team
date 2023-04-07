76291461a3b22118268e60159e6e286df987231d
Seonjin Kim <seonjin6804@gmail.com>
2023-04-08 00:09:11 +0900

    style: move smoothly

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,82 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[System.Serializable]
-public class UnityAnimationEvent : UnityEvent<string> { };
 
 public class CharacterController : MonoBehaviour
 {
+    public float moveTransitionSpeed = 5f;
+    public float jumpForce = 300;
 
-    public float jumpForce = 50;
-
-    private int rail = 0;
-    private bool canMove = true;
+    private int rail = 1;
     private bool canJump = true;
-    private bool canSlide = true;
     private Animator animator;
     private Rigidbody rb;
 
-    public UnityAnimationEvent OnAnimationStart;
-    public UnityAnimationEvent OnAnimationComplete;
+    private List<float> railPos = new List<float>()
+    {
+        -1.5f,
+        0,
+        1.5f
+    };
 
     void Awake()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
-        for (int i = 0; i < animator.runtimeAnimatorController.animationClips.Length; i++)
-        {
-            AnimationClip clip = animator.runtimeAnimatorController.animationClips[i];
-
-            AnimationEvent animationStartEvent = new AnimationEvent();
-            animationStartEvent.time = 0;
-            animationStartEvent.functionName = "AnimationStartHandler";
-            animationStartEvent.stringParameter = clip.name;
-
-            AnimationEvent animationEndEvent = new AnimationEvent();
-            animationEndEvent.time = clip.length;
-            animationEndEvent.functionName = "AnimationCompleteHandler";
-            animationEndEvent.stringParameter = clip.name;
-
-            clip.AddEvent(animationStartEvent);
-            clip.AddEvent(animationEndEvent);
-        }
     }
-
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && rail > -1 && canMove)
-        {
-            rail -= 1;
-            transform.position += new Vector3(-1.5f, 0, 0);
-            StartCoroutine(MoveDelay());
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow) && rail < 1 && canMove)
-        {
-            rail += 1;
-            transform.position += new Vector3(1.5f, 0, 0);
-            StartCoroutine(MoveDelay());
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow) && canJump)
-        {
-            rb.AddForce(new Vector3(0, jumpForce, 0));
-            animator.SetTrigger("Jump");
-            canJump = false;
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow) && canSlide)
-        {
-            animator.SetTrigger("Slide");
-            canSlide = false;
-        }
+        GetInput();
     }
 
-    IEnumerator MoveDelay()
+    void FixedUpdate()
     {
-        canMove = false;
-        yield return new WaitForSeconds(0.5f);
-        canMove = true;
+        Move();
     }
 
 
@@ -87,19 +45,31 @@ public class CharacterController : MonoBehaviour
         canJump = true;
     }
 
-    public void AnimationStartHandler(string name)
+    private void Move()
     {
-        Debug.Log($"{name} animation start.");
-        OnAnimationStart?.Invoke(name);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(railPos[rail], transform.position.y, transform.position.z), Time.deltaTime * moveTransitionSpeed);
     }
-    public void AnimationCompleteHandler(string name)
-    {
-        Debug.Log($"{name} animation complete.");
-        OnAnimationComplete?.Invoke(name);
 
-        if (name == "Slide")
+    private void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && rail > 0)
         {
-            canSlide = true;
+            rail -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && rail < 2)
+        {
+            rail += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && canJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            animator.SetTrigger("Jump");
+            canJump = false;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && !animator.GetCurrentAnimatorStateInfo(0).IsName("Slide"))
+        {
+            rb.AddForce(Vector3.down * jumpForce);
+            animator.SetTrigger("Slide");
         }
     }
 
